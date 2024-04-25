99df430e1dd2f5f17d1e8b15338e3cc5d2c4d240
Michael Oberst <mioberst@gmx.de>
2024-04-25 19:56:00 +0200

    feat: extract components #10

--- src/pages/benefits.tsx
+++ src/pages/benefits.tsx
@@ -1,83 +1,37 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { Image, Pressable, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+
+import ButtonWhite from '@/ovok-ui/button-white';
+import ButtonWithIcon from '@/ovok-ui/button-with-icon';
+import BackgroundCircles from '@/ovok-ui/double-linear-gradient';
 
 export default function Benefits() {
   return (
-    <View className="flex-1">
-      <LinearGradient
-        colors={['#4f506f', '#756233']}
-        start={{ x: 0, y: 0.5 }}
-        end={{ x: 1, y: 0.5 }}
-        className="flex-1"
-      >
-        <Image
-          source={require('../../assets/images/image-two-stars.png')}
-          className="absolute left-[123px] top-[62.84px] rotate-[-87.41deg]"
-        />
-        <View className="mx-6 my-24">
-          <View className="mb-9">
-            <Text className="font-jost text-[24px] font-light text-[white]">
-              Discover
-            </Text>
-            <Text className="font-jost text-[38px] font-bold text-[white]">
-              the Benefits
-            </Text>
-          </View>
-          <View className="mb-6">
-            <Pressable className="flex-row rounded-xl border border-[grey] bg-[#5f6083] p-4">
-              <View className="mr-3">
-                <Image
-                  source={require('../../assets/images/icon-monitoring.png')}
-                />
-              </View>
-              <View>
-                <Text className="text-[white]">
-                  Real Time HEALTH Monitoring
-                </Text>
-              </View>
-            </Pressable>
-          </View>
-          <View className="mb-6">
-            <Pressable className="flex-row rounded-xl border border-[grey] bg-[#5f6083] p-4">
-              <View className="mr-3">
-                <Image source={require('../../assets/images/icon-care.png')} />
-              </View>
-              <View>
-                <Text className="text-[white]">Personalise Clinical CARE</Text>
-              </View>
-            </Pressable>
-          </View>
-          <View className="mb-6">
-            <Pressable className="flex-row rounded-xl border border-[grey] bg-[#5f6083] p-4">
-              <View className="mr-3">
-                <Image source={require('../../assets/images/icon-team.png')} />
-              </View>
-              <View>
-                <Text className="text-[white]">Direct CARE TEAM ACCESS</Text>
-              </View>
-            </Pressable>
-          </View>
-          <View className="mb-6">
-            <Pressable className="flex-row rounded-xl border border-[grey] bg-[#5f6083] p-4">
-              <View className="mr-3">
-                <Image
-                  source={require('../../assets/images/icon-activities.png')}
-                />
-              </View>
-              <View>
-                <Text className="text-[white]">Daily Health Activities</Text>
-              </View>
-            </Pressable>
-          </View>
-          <View>
-            <Pressable className="mt-9 flex-row justify-center rounded-xl bg-[white] py-4">
-              <Text className="text-[18px] font-medium text-[#525490]">
-                Continue
-              </Text>
-            </Pressable>
-          </View>
+    <BackgroundCircles>
+      <Image
+        source={require('../../assets/images/image-two-stars.png')}
+        className="absolute left-[123px] top-[62.84px] rotate-[-87.41deg]"
+      />
+      <View className="mx-6 my-24">
+        <View className="mb-9">
+          <Text className="font-jost text-[24px] font-light text-[white]">
+            Discover
+          </Text>
+          <Text className="font-jost text-[38px] font-bold text-[white]">
+            the Benefits
+          </Text>
         </View>
-      </LinearGradient>
-    </View>
+        <ButtonWithIcon iconName="monitoring">
+          Real time HEALTH monitoring
+        </ButtonWithIcon>
+        <ButtonWithIcon iconName="care">
+          Personalise Clinical CARE
+        </ButtonWithIcon>
+        <ButtonWithIcon iconName="team">Direct CARE TEAM ACCESS</ButtonWithIcon>
+        <ButtonWithIcon iconName="activities">
+          Daily Health Activities
+        </ButtonWithIcon>
+        <ButtonWhite>Continue</ButtonWhite>
+      </View>
+    </BackgroundCircles>
   );
 }
--- src/pages/data-consent.tsx
+++ src/pages/data-consent.tsx
@@ -1,12 +1,15 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
-import { Image, Linking, Pressable, Text, View } from 'react-native';
+import { Image, Linking, Text, View } from 'react-native';
 
+import ButtonWhite from '@/ovok-ui/button-white';
+import BackgroundCircles from '@/ovok-ui/double-linear-gradient';
 import { Checkbox } from '@/ui';
 
 export default function DataConsent() {
   const [termsChecked, setTermsChecked] = useState(false);
   const [privacyChecked, setPrivacyChecked] = useState(false);
+
+  const continueDisabled: boolean = !termsChecked || !privacyChecked;
 
   const handleTermsInput = () => {
     setTermsChecked((prev) => !prev);
@@ -17,79 +20,61 @@ export default function DataConsent() {
   };
 
   return (
-    <View className="flex-1">
-      <LinearGradient
-        colors={['#4f506f', '#756233']}
-        start={{ x: 0, y: 0.5 }}
-        end={{ x: 1, y: 0.5 }}
-        className="flex-1"
-      >
-        <Image
-          source={require('../../assets/images/image-lock.png')}
-          className="absolute left-[67px] top-[104px]"
-        />
-        <View className="mx-9 mt-[350px]">
-          <View>
-            <Text className="mb-3 text-center text-[28px] font-bold leading-loose text-[white]">
-              Your Data, Your Control
-            </Text>
-            <Text className="mb-3 text-center leading-loose text-[white]">
-              We prioritize your privacy. Only collect what's needed to serve
-              you better. Rest assured, your health data is encrypted and
-              secure.
-            </Text>
-          </View>
-          <View className="mt-24">
-            <View className="mb-6 flex-row">
-              <View className="mr-3">
-                <Checkbox
-                  checked={termsChecked}
-                  onChange={handleTermsInput}
-                  accessibilityLabel="I agree to terms and conditions"
-                />
-              </View>
-              <View>
-                <Text className="text-[white]">
-                  {'I agree to '}
-                  <Text
-                    className="underline"
-                    onPress={() => Linking.openURL('')}
-                  >
-                    Terms & Conditions.
-                  </Text>
-                </Text>
-              </View>
+    <BackgroundCircles>
+      <Image
+        source={require('../../assets/images/image-lock.png')}
+        className="absolute left-[67px] top-[104px]"
+      />
+      <View className="mx-9 mt-[350px]">
+        <View>
+          <Text className="mb-3 text-center text-[28px] font-bold leading-loose text-[white]">
+            Your Data, Your Control
+          </Text>
+          <Text className="mb-3 text-center leading-loose text-[white]">
+            We prioritize your privacy. Only collect what's needed to serve you
+            better. Rest assured, your health data is encrypted and secure.
+          </Text>
+        </View>
+        <View className="mt-24">
+          <View className="mb-6 flex-row">
+            <View className="mr-3">
+              <Checkbox
+                checked={termsChecked}
+                onChange={handleTermsInput}
+                accessibilityLabel="I agree to terms and conditions"
+              />
             </View>
-            <View className="flex-row">
-              <View className="mr-3">
-                <Checkbox
-                  checked={privacyChecked}
-                  onChange={handlePrivacyInput}
-                  accessibilityLabel="I agree to privacy policy"
-                />
-              </View>
-              <View>
-                <Text className="text-[white]">
-                  {'I agree to '}
-                  <Text
-                    className="underline"
-                    onPress={() => Linking.openURL('')}
-                  >
-                    Privacy Policy.
-                  </Text>
+            <View>
+              <Text className="text-[white]">
+                {'I agree to '}
+                <Text className="underline" onPress={() => Linking.openURL('')}>
+                  Terms & Conditions.
                 </Text>
-              </View>
-            </View>
-          </View>
-          <View>
-            <Pressable className="mt-9 flex-row justify-center rounded-xl bg-[white] py-4">
-              <Text className="text-[18px] font-medium text-[#525490]">
-                Continue
               </Text>
-            </Pressable>
+            </View>
+          </View>
+          <View className="flex-row">
+            <View className="mr-3">
+              <Checkbox
+                checked={privacyChecked}
+                onChange={handlePrivacyInput}
+                accessibilityLabel="I agree to privacy policy"
+              />
+            </View>
+            <View>
+              <Text className="text-[white]">
+                {'I agree to '}
+                <Text className="underline" onPress={() => Linking.openURL('')}>
+                  Privacy Policy.
+                </Text>
+              </Text>
+            </View>
           </View>
         </View>
-      </LinearGradient>
-    </View>
+        <View>
+          <ButtonWhite disabled={continueDisabled}>Continue</ButtonWhite>
+        </View>
+      </View>
+    </BackgroundCircles>
   );
 }
