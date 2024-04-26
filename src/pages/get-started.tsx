576b98871a667c42277e18bcea67585195035ee5
Michael Oberst <mioberst@gmx.de>
2024-04-26 11:57:45 +0200

    feat: add get-started page #10

--- src/pages/get-started.tsx
+++ src/pages/get-started.tsx
@@ -1,18 +1,10 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react';
 import { Text, View } from 'react-native';
 
-import type { Option } from '@/ui';
-import { Input, Select } from '@/ui';
-
-const areaCodeOptions: Option[] = [
-  { value: '+233', label: '+233' },
-  { value: '+49', label: '+49' },
-];
+import ButtonWhite from '@/ovok-ui/button-white';
+import { Input } from '@/ui';
 
 export default function GetStarted() {
-  const [areaCode, setAreaCode] = useState(areaCodeOptions[0].value);
-
   return (
     <View className="flex-1">
       <LinearGradient
@@ -21,25 +13,35 @@ export default function GetStarted() {
         end={{ x: 1, y: 0.5 }}
         className="flex-1"
       >
-        <View className="mx-9 mt-[350px]">
-          <View>
-            <Text className="mb-3 text-center font-jost font-bold text-[white]">
+        <View className="mx-9 mt-12">
+          <View className="mb-9">
+            <Text className="mb-3 font-jost text-[28px] font-bold leading-[1.8] text-[white]">
               Get Started!
             </Text>
-            <Text className="mb-6 text-center font-jost text-[white]">
-              Getting started is just a tap away. Input your number and we'll
-              guide you forward.
+            <Text className="font-jost leading-[1.8] text-[white]">
+              Getting started is just a tap away. Input your email and a
+              password and we'll guide you forward.
             </Text>
           </View>
-          <View className="flex-row">
-            <Select
-              label="Select"
-              options={areaCodeOptions}
-              value={areaCode}
-              onSelect={(option) => setAreaCode(option)}
+          <View className="mb-3 w-full">
+            <Input
+              placeholder="Enter your email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              className="rounded-xl border border-[rgba(255,255,255,0.3)] bg-[rgba(255,255,255,0.06)] py-4 pl-6 text-[white]"
             />
-            <Input label="phone" placeholder="Enter your number" />
           </View>
+          <View className="w-full">
+            <Input
+              placeholder="Select a password"
+              keyboardType="visible-password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              className="rounded-xl border border-[rgba(255,255,255,0.3)] bg-[rgba(255,255,255,0.06)] py-4 pl-6 text-[white]"
+            />
+          </View>
+          <ButtonWhite>Continue</ButtonWhite>
         </View>
       </LinearGradient>
     </View>
