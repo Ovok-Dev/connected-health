bf65a3fd5003d7552c0145fdb269ac0dfe34a43f
Michael Oberst <mioberst@gmx.de>
2024-05-13 13:08:21 +0200

    feat: fix add-medication #10

--- src/pages/add-medication.tsx
+++ src/pages/add-medication.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Image, Pressable, Text, TextInput, View } from 'react-native';
 import type { DateData } from 'react-native-calendars';
 import { Calendar } from 'react-native-calendars';
-import NumericInput from 'react-native-numeric-input';
 
 import BackgroundWhite from '@/ovok-ui/background-white';
 import ButtonColorful from '@/ovok-ui/button-colorful';
@@ -12,7 +11,7 @@ export default function AddMedication() {
   const [medicineName, setMedicineName] = useState<string>('');
   const [startDate, setStartDate] = useState<string>('');
   const [endDate, setEndDate] = useState<string>('');
-  const [frequency, setFrequency] = useState<number>(0);
+  const [frequency, setFrequency] = useState<string>('');
   const [dose, setDose] = useState<string>('');
   const [startDateCalendarOpen, setStartDateCalendarOpen] =
     useState<boolean>(false);
@@ -22,81 +21,89 @@ export default function AddMedication() {
   const saveButtonDisabled: boolean =
     medicineName && startDate && endDate && frequency && dose ? false : true;
 
-  const handleStartDateDayPress = (dateObject: DateData) => {
-    setStartDate(dateObject.dateString);
+  const handleStartDateCalendarButton = () => {
+    setStartDateCalendarOpen((prev) => !prev);
+    setEndDateCalendarOpen(false);
+  };
+
+  const handleEndDateCalendarButton = () => {
+    setEndDateCalendarOpen((prev) => !prev);
     setStartDateCalendarOpen(false);
   };
 
-  const handleEndDateDayPress = (dateObject: DateData) => {
-    setEndDate(dateObject.dateString);
+  const handleDayPress = (dateObject: DateData) => {
+    if (startDateCalendarOpen) {
+      setStartDate(dateObject.dateString);
+    } else if (endDateCalendarOpen) {
+      setEndDate(dateObject.dateString);
+    }
+    setStartDateCalendarOpen(false);
     setEndDateCalendarOpen(false);
   };
 
   return (
     <BackgroundWhite>
-      <View className="flex-1">
+      <View className="mt-3">
         <Text className="text-[14px]">Medicine Name</Text>
         <TextInput
           value={medicineName}
           onChangeText={(value) => setMedicineName(value)}
           autoCorrect={false}
-          className="rounded-lg bg-[white]"
+          className="mt-2 h-[40px] rounded-lg bg-[white] px-3"
         />
       </View>
-      <View className="flex-1 flex-row gap-3">
+      <View className="my-3 flex-1 flex-row justify-center gap-3">
         <View className="flex-1">
           <Text className="text-[14px]">Start Date</Text>
-          <Pressable className="flex-1 flex-row rounded-lg bg-[white] p-2">
+          <Pressable
+            className="mt-2 flex-1 flex-row rounded-lg bg-[white] p-2"
+            onPress={handleStartDateCalendarButton}
+          >
             <Image source={getIcon('calendar')} width={20} height={20} />
-            <Text className="ml-2 text-[14px]">
+            <Text className="ml-2 text-[14px] leading-normal">
               {startDate ? startDate : 'DD/MM'}
             </Text>
           </Pressable>
-          {startDateCalendarOpen && (
-            <Calendar onDayPress={handleStartDateDayPress} />
-          )}
         </View>
-      </View>
-      <View className="flex-1 flex-row gap-3">
         <View className="flex-1">
           <Text className="text-[14px]">End Date</Text>
-          <Pressable className="flex-1 flex-row rounded-lg bg-[white] p-2">
+          <Pressable
+            className="align-center mt-2 flex-1 flex-row rounded-lg bg-[white] p-2"
+            onPress={handleEndDateCalendarButton}
+          >
             <Image source={getIcon('calendar')} width={20} height={20} />
-            <Text className="ml-2 text-[14px]">
+            <Text className="ml-2 text-[14px] leading-normal">
               {endDate ? endDate : 'DD/MM'}
             </Text>
           </Pressable>
-          {endDateCalendarOpen && (
-            <Calendar onDayPress={handleEndDateDayPress} />
-          )}
         </View>
       </View>
-      <View className="flex-1 flex-row gap-3">
+      {(startDateCalendarOpen || endDateCalendarOpen) && (
+        <Calendar onDayPress={handleDayPress} />
+      )}
+      <View className="my-3 flex-1 flex-row gap-3">
         <View className="flex-1">
-          <Text className="text-[14px]">Frequency</Text>
-          <NumericInput
-            type="up-down"
-            valueType="integer"
+          <Text className="text-[14px]">Frequency (per day)</Text>
+          <TextInput
             value={frequency}
-            onChange={(value) => setFrequency(value)}
-            containerStyle={{
-              backgroundColor: 'white',
-              borderRadius: 8,
-            }}
+            onChangeText={(value) => setFrequency(value)}
+            autoCorrect={false}
+            keyboardType="numeric"
+            className="mt-2 h-[40px] rounded-lg bg-[white] px-3"
           />
         </View>
         <View className="flex-1">
-          <Text className="text-[14px]">Frequency</Text>
+          <Text className="text-[14px]">Dose (mg)</Text>
           <TextInput
             value={dose}
             onChangeText={(value) => setDose(value)}
             autoCorrect={false}
             keyboardType="numeric"
-            className="rounded-lg bg-[white]"
+            className="mt-2 h-[40px] rounded-lg bg-[white] px-3"
           />
         </View>
-        <ButtonColorful disabled={saveButtonDisabled}>Save</ButtonColorful>
       </View>
+      <ButtonColorful disabled={saveButtonDisabled}>Save</ButtonColorful>
     </BackgroundWhite>
   );
 }
