06c1ea0c75c41d65efdb45508d064080394c77d9
Michael Oberst <mioberst@gmx.de>
2024-05-13 15:56:25 +0200

    feat: add questionnaires #10

---
+++ src/ovok-ui/button-questionnaires.tsx
@@ -0,0 +1,49 @@
+import { Dimensions, Image, Pressable, Text } from 'react-native';
+
+import { getIcon } from '@/utils/get-icon';
+
+interface Props {
+  title: string;
+  subtitle: string;
+  isNew?: boolean;
+  completed?: boolean;
+}
+
+export default function ButtonQuestionnaires({
+  title,
+  subtitle,
+  isNew = false,
+  completed = false,
+}: Props) {
+  const { width } = Dimensions.get('window');
+
+  return (
+    <Pressable
+      className="my-3 flex-1 items-start gap-2 rounded-lg p-3 pr-6"
+      style={{
+        backgroundColor: completed ? 'rgb(249,255,245)' : 'white',
+        borderWidth: completed ? 1 : 0,
+        borderColor: 'rgba(100,204,39,0.45)',
+      }}
+    >
+      {isNew && (
+        <Text className="rounded-sm bg-[rgb(248,191,39)] px-2 py-1 text-[white]">
+          New
+        </Text>
+      )}
+      <Text className="text-[14px] font-semibold leading-[1.8] text-[rgb(14,16,18)]">
+        {title}
+      </Text>
+      <Text className="text-[12px] text-[rgb(74,84,94)]">{subtitle}</Text>
+      {completed && (
+        <Image
+          source={getIcon('checkmark')}
+          width={24}
+          height={24}
+          className="absolute"
+          style={{ left: width - 54 }}
+        />
+      )}
+    </Pressable>
+  );
+}
---
+++ src/pages/questionnaires.tsx
@@ -0,0 +1,23 @@
+import BackgroundWhite from '@/ovok-ui/background-white';
+import ButtonQuestionnaires from '@/ovok-ui/button-questionnaires';
+
+export default function Questionnaires() {
+  return (
+    <BackgroundWhite>
+      <ButtonQuestionnaires
+        title="Hypertension Experience Feedback Tool (HEFT)"
+        subtitle="5 Questions"
+        isNew
+      />
+      <ButtonQuestionnaires
+        title="Treatment Satisfaction Questionnaire for Medication (TSQM)"
+        subtitle="5 Questions"
+      />
+      <ButtonQuestionnaires
+        title="The Morisky Medication Adherence Scale (MMAS)"
+        subtitle="8 Gauge"
+        completed
+      />
+    </BackgroundWhite>
+  );
+}
