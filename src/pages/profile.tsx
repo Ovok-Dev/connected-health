12bcc41bbfc040ae3c8e76e612bf67035da10783
Michael Oberst <mioberst@gmx.de>
2024-05-16 13:35:41 +0200

    feat: add header of profile page #10

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,5 @@
-import { Image, Pressable, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image, ImageBackground, Pressable, Text, View } from 'react-native';
 
 import BackgroundWhite from '@/ovok-ui/background-white';
 import ButtonBasic from '@/ovok-ui/button-basic';
@@ -7,6 +8,49 @@ import { getIcon } from '@/utils/get-icon';
 export default function Profile() {
   return (
     <BackgroundWhite>
+      <View className="mb-6 overflow-hidden rounded-xl">
+        <LinearGradient
+          colors={['rgba(82, 83, 146, 1)', 'rgba(238, 185, 51, 1)']}
+          start={{ x: -0.4, y: 1 }}
+          end={{ x: 1.4, y: 1.2 }}
+          locations={[0.1791, 1.691]}
+          className="flex-1"
+        >
+          <LinearGradient
+            colors={[
+              'rgba(82, 84, 144, 1)',
+              'rgba(82, 84, 144, 0)',
+              'rgba(93, 136, 144, 0.75)',
+            ]}
+            start={{ x: 1, y: 0 }}
+            end={{ x: 0, y: 3.4823 }}
+            locations={[-0.0175, -0.0174, 1]}
+            className="flex-1"
+          >
+            <ImageBackground
+              source={require('../../assets/images/background-image-benefits.png')}
+              resizeMode="cover"
+              className="flex-1"
+            >
+              <View className="h-[190px] items-center justify-center gap-2">
+                <View className="h-[72px] w-[72px] overflow-hidden rounded-full bg-white">
+                  <Image
+                    source={getIcon('grace')}
+                    className="w-full flex-1"
+                    resizeMode="contain"
+                  />
+                </View>
+                <Text className="text-[24px] font-medium text-white">
+                  Grace Agyei
+                </Text>
+                <Text className="text-[12px] text-white opacity-75">
+                  example@domain.com
+                </Text>
+              </View>
+            </ImageBackground>
+          </LinearGradient>
+        </LinearGradient>
+      </View>
       <ButtonBasic
         title="Personal Information"
         iconNameLeft="user"
