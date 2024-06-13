d1557e20796a9cdb648d1de9ff419bfde144b68c
Michael Oberst <mioberst@gmx.de>
2024-06-13 07:59:54 +0200

    fix: put video request into useEffect #10

--- src/app/(tabs)/(home)/video-call.tsx
+++ src/app/(tabs)/(home)/video-call.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import { VideoCallService } from '@/api/common/video-call.service';
@@ -6,25 +7,29 @@ import { VideoCallService } from '@/api/common/video-call.service';
 export default function VideoCall() {
   const [videoLink, setVideoLink] = useState<string>('');
 
-  const videoCallService = new VideoCallService();
-  videoCallService
-    .createVideoCall()
-    .then((data) => {
-      setVideoLink(data.participants?.[0]?.callUrl);
-    })
-    .catch((error) => console.log(error));
+  useEffect(() => {
+    const videoCallService = new VideoCallService();
+    videoCallService
+      .createVideoCall()
+      .then((data) => {
+        setVideoLink(data.participants?.[0]?.callUrl);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-  return (
-    videoLink && (
-      <WebView
-        source={{ uri: videoLink }}
-        className="flex-1"
-        geolocationEnabled={true}
-        mediaPlaybackRequiresUserAction={false}
-        javaScriptEnabled={true}
-        startInLoadingState={true}
-        scalesPageToFit={true}
-      />
-    )
+  return !videoLink ? (
+    <View className="flex-1 items-center justify-center">
+      <ActivityIndicator size="small" color="grey" />
+    </View>
+  ) : (
+    <WebView
+      source={{ uri: videoLink }}
+      className="flex-1"
+      geolocationEnabled={true}
+      mediaPlaybackRequiresUserAction={false}
+      javaScriptEnabled={true}
+      startInLoadingState={true}
+      scalesPageToFit={true}
+    />
   );
 }
